12ea7f09f3a696f370bec39dc4c93a5b95774752
zyx <1163003193@qq.com>
2019-05-10 16:54:09 +0800

    不明觉厉

---
+++ shareInformation/src/test/java/com/zyx/test/UserTest.java
@@ -0,0 +1,10 @@
+package com.zyx.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@RunWith(SpringRunner::class)
+@SpringBootTest
+public class UserTest {
+
+}
